5a419a520181bee3efe65b3bfbcf334cae297f83
TongJetKit <jton0028@student.monash.edu>
2023-04-19 01:21:51 +0800

    Created 2 action class which is the skill actions that Uchigatana and Great Knife can do

---
+++ src/game/UnsheatheAction.java
@@ -0,0 +1,43 @@
+package game;
+
+import edu.monash.fit2099.engine.actions.Action;
+import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.positions.GameMap;
+import edu.monash.fit2099.engine.weapons.Weapon;
+
+import java.util.Random;
+
+public class UnsheatheAction extends AttackAction{
+
+    /**
+     * Constructor for the unsheathe action skill for Uchigatana where the damage is doubled and has a 60% chance of hitting
+     * @param target The target of the skill
+     * @param direction The direction of the actor
+     * @param weapon The weapon used to attack
+     */
+    public UnsheatheAction(Actor target, String direction, Weapon weapon) {
+        super(target, direction, weapon);
+    }
+
+    @Override
+    public String execute(Actor actor, GameMap map) {
+
+        Weapon weapon = getWeapon();
+        Actor target = getTarget();
+
+
+        if (!(getRand().nextInt(100) <= weapon.chanceToHit())) {
+            return actor + " misses " + target + ".";
+        }
+
+        int damage =  weapon.damage();
+        String result = actor + " " + weapon.verb() + " " + target + " for " + damage + " damage.";
+        target.hurt(damage);
+
+        if (!target.isConscious()) {
+            result += new DeathAction(actor).execute(target, map);
+        }
+
+        return result;
+    }
+}
---
+++ src/game/weapons/MoveStabAction.java
@@ -0,0 +1,55 @@
+package game.weapons;
+
+import edu.monash.fit2099.engine.actions.MoveActorAction;
+import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.positions.Exit;
+import edu.monash.fit2099.engine.positions.GameMap;
+import edu.monash.fit2099.engine.positions.Location;
+import edu.monash.fit2099.engine.weapons.Weapon;
+import game.AttackAction;
+import game.DeathAction;
+
+import java.util.List;
+import java.util.Random;
+
+public class MoveStabAction extends AttackAction {
+    public MoveStabAction(Actor target, String direction, Weapon weapon) {
+        super(target, direction, weapon);
+    }
+
+    @Override
+    public String execute(Actor actor, GameMap map) {
+        Weapon weapon = getWeapon();
+        Actor target = getTarget();
+        Random rand = getRand();
+
+
+
+
+
+        if (!(getRand().nextInt(100) <= weapon.chanceToHit())) {
+            return actor + " misses " + target + ".";
+        }
+
+        Location currentLocation = map.locationOf(actor);
+        List<Exit> exits = currentLocation.getExits();
+
+        int damage =  weapon.damage();
+        String result = actor + " " + weapon.verb() + " " + target + " for " + damage + " damage.";
+        target.hurt(damage);
+
+        if(!exits.isEmpty()){
+            Exit exit = exits.get(rand.nextInt(exits.size()));
+            Location move = exit.getDestination();
+            result += new MoveActorAction(move,exit.getName()).execute(actor,map);
+        }
+
+        if (!target.isConscious()) {
+            result += new DeathAction(actor).execute(target, map);
+        }
+
+
+
+        return result;
+    }
+}
