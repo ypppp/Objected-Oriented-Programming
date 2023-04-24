a0c97df9a219c282256afe52a0bebc296bd8071d
TongJetKit <jton0028@student.monash.edu>
2023-04-24 20:35:14 +0800

    Created the PileOfBones class and implement it

--- src/game/entity/enemies/PileOfBones.java
+++ src/game/entity/enemies/PileOfBones.java
@@ -1,21 +1,47 @@
 package game.entity.enemies;
 
 
+import edu.monash.fit2099.engine.actions.Action;
+import edu.monash.fit2099.engine.actions.ActionList;
+import edu.monash.fit2099.engine.actions.DoNothingAction;
+import edu.monash.fit2099.engine.displays.Display;
+import edu.monash.fit2099.engine.positions.GameMap;
+import edu.monash.fit2099.engine.positions.Location;
+import game.Status;
+
 import java.util.HashMap;
 
 
 public class PileOfBones extends Enemy{
 
-
+    private int counter = 3;
+    private Enemy revivedEnemy;
 
     /**
      * Constructor.
      */
-    public PileOfBones() {
+    public PileOfBones(Enemy enemy) {
         super("Pile of Bones", 'X',1);
         this.setBehaviours(new HashMap<>());
+        this.addCapability(Status.REVIVABLE);
+        this.revivedEnemy = enemy;
 
 
     }
 
+    @Override
+    public Action playTurn(ActionList actions, Action lastAction, GameMap map, Display display) {
+        // if counter becomes 0 then revive to the revivedEnemy
+        // by removing the actor on that location then adding the revivedEnemy.
+
+        if(counter==0){
+            Location here = map.locationOf(this);
+            map.removeActor(this);
+            map.addActor(revivedEnemy, here);
+        }
+
+        counter--;
+
+        return new DoNothingAction();
+    }
 }
