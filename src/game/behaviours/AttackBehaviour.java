2a4d4888c16ca8807a878464be153b4fd00c7670
TongJetKit <jton0028@student.monash.edu>
2023-04-24 14:11:08 +0800

    Created an AttackBehaviour class

---
+++ src/game/behaviours/AttackBehaviour.java
@@ -0,0 +1,12 @@
+package game.behaviours;
+
+import edu.monash.fit2099.engine.actions.Action;
+import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.positions.GameMap;
+
+public class AttackBehaviour implements Behaviour {
+    @Override
+    public Action getAction(Actor actor, GameMap map) {
+        return null;
+    }
+}
