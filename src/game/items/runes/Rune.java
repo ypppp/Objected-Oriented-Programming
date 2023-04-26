6c92b39a08d52d2b9585e90b14724e9caf3d0edc
TongJetKit <jton0028@student.monash.edu>
2023-04-26 19:06:49 +0800

    Created the trader, rune and runemanager class

--- src/game/items/runes/Rune.java
+++ src/game/items/runes/Rune.java
@@ -1,4 +1,25 @@
 package game.items.runes;
 
-public class Rune {
+import edu.monash.fit2099.engine.items.Item;
+
+public class Rune extends Item {
+
+    private int amount;
+
+
+    /***
+     * Constructor.
+     */
+    public Rune() {
+        super("Rune", '$', false);
+        this.amount = 0;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = Math.max(amount,0);
+    }
 }
