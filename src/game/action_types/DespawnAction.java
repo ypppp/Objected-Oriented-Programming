1ebb8a13f5f6bc3d815aeed17e81707ab7b4a9b9
TongJetKit <jton0028@student.monash.edu>
2023-04-26 19:05:51 +0800

    Added docstring for DespawnAction

--- src/game/action_types/DespawnAction.java
+++ src/game/action_types/DespawnAction.java
@@ -6,12 +6,26 @@ import edu.monash.fit2099.engine.positions.GameMap;
 
 public class DespawnAction extends Action {
 
+    /**
+     * Perform the Action.
+     *
+     * @param actor The actor performing the action.
+     * @param map   The map the actor is on.
+     * @return a description of what happened that can be displayed to the user.
+     */
     @Override
     public String execute(Actor actor, GameMap map) {
         map.removeActor(actor);
         return menuDescription(actor);
     }
 
+
+    /**
+     * Returns a descriptive string
+     *
+     * @param actor The actor performing the action.
+     * @return the text we put on the menu
+     */
     @Override
     public String menuDescription(Actor actor) {
         return actor + " despawned";
