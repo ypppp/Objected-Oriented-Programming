9fc59799bf8dd0b5670798f8e8e8cfc35286e696
TongJetKit <jton0028@student.monash.edu>
2023-04-26 19:04:46 +0800

    Add status that the club can be purchasable/sellable

--- src/game/weapons/playerweapons/Club.java
+++ src/game/weapons/playerweapons/Club.java
@@ -3,6 +3,7 @@ package game.weapons.playerweapons;
 import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.positions.Location;
 import edu.monash.fit2099.engine.weapons.WeaponItem;
+import game.Status;
 
 /**
  * A simple weapon that can be used to attack the enemy.
@@ -19,6 +20,8 @@ public class Club extends WeaponItem {
      */
     public Club() {
         super("Club", '!', 103, "bonks", 80);
+        this.addCapability(Status.SELLABLE);
+        this.addCapability(Status.PURCHASABLE);
     }
 
     @Override
