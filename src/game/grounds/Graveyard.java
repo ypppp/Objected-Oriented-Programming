06476e819300ddbd7d216256b9c64dd6333c8dde
YewYeePerng <yeeperngyew@gmail.com>
2023-04-28 20:02:16 +0800

    Modified Graveyard to take account of east and west side of the map for spawning Heavy Skeleton Swordsman or SkeletonBandit.

--- src/game/grounds/Graveyard.java
+++ src/game/grounds/Graveyard.java
@@ -3,9 +3,7 @@ package game.grounds;
 import edu.monash.fit2099.engine.positions.Ground;
 import edu.monash.fit2099.engine.positions.Location;
 import game.RandomNumberGenerator;
-import game.entity.enemies.GiantCrab;
-import game.entity.enemies.HeavySkeletonSwordsman;
-import game.entity.enemies.LoneWolf;
+import game.entity.enemies.*;
 
 /**
  * Created by:
@@ -21,9 +19,11 @@ public class Graveyard extends Ground {
     }
 
     public void tick(Location location) {
-        if(RandomNumberGenerator.getRandomInt(100)<28 && !location.containsAnActor()){
-            location.addActor(new HeavySkeletonSwordsman());
-        }
+        if (location.x() > 40 && (RandomNumberGenerator.getRandomInt(100)<28 && !location.containsAnActor())){
+            location.addActor(new SkeletonBandit());}
+        else if ((RandomNumberGenerator.getRandomInt(100)<28 && !location.containsAnActor()))
+        {
+            location.addActor(new HeavySkeletonSwordsman());}
 
     }
 }
