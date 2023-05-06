e8cea5077b4f4406c2ddcd055745f888c62cbcd1
YewYeePerng <yeeperngyew@gmail.com>
2023-05-06 17:54:30 +0800

    Added Java docs for SkeletonBandit

--- src/game/entity/enemies/SkeletonBandit.java
+++ src/game/entity/enemies/SkeletonBandit.java
@@ -11,7 +11,16 @@ import game.weapons.enemyweapons.Scimitar;
 
 import java.util.ArrayList;
 
+/**
+ * The giant crab that spawns in the puddle of water
+ * @author Yew Yee Perng
+ * @see Enemy
+ */
+
 public class SkeletonBandit extends Enemy{
+    /**
+     * Constructor.
+     */
     public SkeletonBandit() {
         super("Skeleton Bandit", 'b',184);
         this.addCapability(Status.HOSTILE_TO_PLAYER);
@@ -20,7 +29,10 @@ public class SkeletonBandit extends Enemy{
         weapon.togglePortability();
         this.addWeaponToInventory(weapon);
     }
-
+    /**
+     * Creates and returns an intrinsic weapon.
+     * @return A newly instantiated intrinsic weapon
+     */
     @Override
     public IntrinsicWeapon getIntrinsicWeapon() {
         return new IntrinsicWeapon(80, "punches", 85);
