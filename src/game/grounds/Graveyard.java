3725992df251f589dea99210af4b323718f20553
YewYeePerng <yeeperngyew@gmail.com>
2023-05-06 17:53:14 +0800

    Added Java docs for Graveyard

--- src/game/grounds/Graveyard.java
+++ src/game/grounds/Graveyard.java
@@ -6,12 +6,14 @@ import edu.monash.fit2099.engine.positions.Location;
 /**
  * Created by:
  * @author Aaren Wong Cong Ming
- * Modified by:
+ * Modified by: Yew Yee Perng
  *
  */
 
 public class Graveyard extends SpawningGrounds {
-
+    /**
+     * Constructor.
+     */
 
     public Graveyard() {
         super('n');
