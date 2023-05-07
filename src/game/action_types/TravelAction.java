d115988e5819389f33289601d07fcede16cf8eb2
TongJetKit <jton0028@student.monash.edu>
2023-05-08 02:19:02 +0800

    Created the door ground and the action to travel to

---
+++ src/game/action_types/TravelAction.java
@@ -0,0 +1,43 @@
+package game.action_types;
+
+import edu.monash.fit2099.engine.actions.Action;
+import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.positions.GameMap;
+import edu.monash.fit2099.engine.positions.Location;
+
+public class TravelAction extends Action {
+
+    private Location destination;
+
+    private String name;
+
+
+    public TravelAction(Location destination, String name) {
+        this.destination = destination;
+        this.name = name;
+    }
+
+    /**
+     * Perform the Action.
+     *
+     * @param actor The actor performing the action.
+     * @param map   The map the actor is on.
+     * @return a description of what happened that can be displayed to the user.
+     */
+    @Override
+    public String execute(Actor actor, GameMap map) {
+        map.moveActor(actor,destination);
+        return menuDescription(actor);
+    }
+
+    /**
+     * Returns a descriptive string
+     *
+     * @param actor The actor performing the action.
+     * @return the text we put on the menu
+     */
+    @Override
+    public String menuDescription(Actor actor) {
+        return actor + " travel to " + name;
+    }
+}
---
+++ src/game/grounds/Door.java
@@ -0,0 +1,33 @@
+package game.grounds;
+
+import edu.monash.fit2099.engine.actions.ActionList;
+import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.positions.Ground;
+import edu.monash.fit2099.engine.positions.Location;
+import game.action_types.TravelAction;
+
+public class Door extends Ground {
+
+    private Location travelDestination;
+    private String destinationName;
+
+    /**
+     * Constructor.
+     */
+    public Door(Location travelDestination, String destinationName) {
+        super('D');
+        this.travelDestination = travelDestination;
+        this.destinationName = destinationName;
+    }
+
+    public Location getTravelDestination() {
+        return travelDestination;
+    }
+
+    @Override
+    public ActionList allowableActions(Actor actor, Location location, String direction) {
+        ActionList actions = new ActionList();
+        actions.add(new TravelAction(getTravelDestination(),destinationName));
+        return actions;
+    }
+}
