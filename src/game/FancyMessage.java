3768f8927cac38c6928bd3513127dc9c74a18069
TongJetKit <jton0028@student.monash.edu>
2023-05-08 02:09:18 +0800

    Added the Discovered message for when lost of site grace is found

--- src/game/FancyMessage.java
+++ src/game/FancyMessage.java
@@ -26,4 +26,19 @@ public class FancyMessage {
             "     MM    MM.      ,MP MM       M        MM     ,MP MM    MM   Y  ,   MM     ,MP \n" +
             "     MM    `Mb.    ,dP' YM.     ,M        MM    ,dP' MM    MM     ,M   MM    ,dP' \n" +
             "   .JMML.    `\"bmmd\"'    `bmmmmd\"'      .JMMmmmdP' .JMML..JMMmmmmMMM .JMMmmmdP'   \n";
+
+    public static String DISCOVERED =
+            "\n" +
+                    "                                                                                                                                                                                                                                     \n" +
+                    "                                                                                                                                                                                                                                     \n" +
+                    "`7MMF'        .g8\"\"8q.    .M\"\"\"bgd MMP\"\"MM\"\"YMM       .g8\"\"\"bgd `7MM\"\"\"Mq.        db       .g8\"\"\"bgd `7MM\"\"\"YMM      `7MM\"\"\"Yb. `7MMF' .M\"\"\"bgd   .g8\"\"\"bgd   .g8\"\"8q.`7MMF'   `7MF'`7MM\"\"\"YMM  `7MM\"\"\"Mq.  `7MM\"\"\"YMM  `7MM\"\"\"Yb.   \n" +
+                    "  MM        .dP'    `YM. ,MI    \"Y P'   MM   `7     .dP'     `M   MM   `MM.      ;MM:    .dP'     `M   MM    `7        MM    `Yb. MM  ,MI    \"Y .dP'     `M .dP'    `YM.`MA     ,V    MM    `7    MM   `MM.   MM    `7    MM    `Yb. \n" +
+                    "  MM        dM'      `MM `MMb.          MM          dM'       `   MM   ,M9      ,V^MM.   dM'       `   MM   d          MM     `Mb MM  `MMb.     dM'       ` dM'      `MM VM:   ,V     MM   d      MM   ,M9    MM   d      MM     `Mb \n" +
+                    "  MM        MM        MM   `YMMNq.      MM          MM            MMmmdM9      ,M  `MM   MM            MMmmMM          MM      MM MM    `YMMNq. MM          MM        MM  MM.  M'     MMmmMM      MMmmdM9     MMmmMM      MM      MM \n" +
+                    "  MM      , MM.      ,MP .     `MM      MM          MM.    `7MMF' MM  YM.      AbmmmqMA  MM.           MM   Y  ,       MM     ,MP MM  .     `MM MM.         MM.      ,MP  `MM A'      MM   Y  ,   MM  YM.     MM   Y  ,   MM     ,MP \n" +
+                    "  MM     ,M `Mb.    ,dP' Mb     dM      MM          `Mb.     MM   MM   `Mb.   A'     VML `Mb.     ,'   MM     ,M       MM    ,dP' MM  Mb     dM `Mb.     ,' `Mb.    ,dP'   :MM;       MM     ,M   MM   `Mb.   MM     ,M   MM    ,dP' \n" +
+                    ".JMMmmmmMMM   `\"bmmd\"'   P\"Ybmmd\"     .JMML.          `\"bmmmdPY .JMML. .JMM..AMA.   .AMMA. `\"bmmmd'  .JMMmmmmMMM     .JMMmmmdP' .JMML.P\"Ybmmd\"    `\"bmmmd'    `\"bmmd\"'      VF      .JMMmmmmMMM .JMML. .JMM..JMMmmmmMMM .JMMmmmdP'   \n" +
+                    "                                                                                                                                                                                                                                     \n" +
+                    "                                                                                                                                                                                                                                     \n" +
+                    "\n";
 }
