2248999a0247673cb78f69796afa90b53cee4bf5
YewYeePerng <yeeperngyew@gmail.com>
2023-05-11 22:48:35 +0800

    Added Javadocs for Cage

--- src/game/grounds/Cage.java
+++ src/game/grounds/Cage.java
@@ -1,6 +1,15 @@
 package game.grounds;
 
+/**
+ * The Cage class which is one of the spawning grounds in the game
+ * @author Yew Yee Perng
+ * @see SpawningGrounds
+ * @version 1.0
+ */
 public class Cage extends SpawningGrounds{
+    /**
+     * Constructor.
+     */
     public Cage() {
         super('<');
     }
