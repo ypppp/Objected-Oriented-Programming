608e02539cda27adb846f250f1db2cf5839eabfa
YewYeePerng <yeeperngyew@gmail.com>
2023-05-11 22:48:22 +0800

    Added Javadocs for Barrack

--- src/game/grounds/Barrack.java
+++ src/game/grounds/Barrack.java
@@ -1,6 +1,15 @@
 package game.grounds;
 
+/**
+ * The Barrack class which is one of the spawning grounds in the game
+ * @author Yew Yee Perng
+ * @see SpawningGrounds
+ * @version 1.0
+ */
 public class Barrack extends SpawningGrounds{
+    /**
+     * Constructor.
+     */
     public Barrack() {
         super('B');
     }
