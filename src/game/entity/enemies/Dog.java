e0cc76d0076f33f650da633c20ed25db2e6af0fb
YewYeePerng <yeeperngyew@gmail.com>
2023-05-11 22:44:35 +0800

    Added Javadocs for dogs

--- src/game/entity/enemies/Dog.java
+++ src/game/entity/enemies/Dog.java
@@ -4,7 +4,17 @@ import edu.monash.fit2099.engine.weapons.IntrinsicWeapon;
 import game.Species;
 import game.Status;
 
+/**
+ * The dog that spawns from cage
+ * @author Yew Yee Perng
+ * @version 1.0
+ * @see Enemy
+ */
+
 public class Dog extends Enemy{
+    /**
+     * Constructor.
+     */
     public Dog() {
         super("Dog", 'a', 104);
         this.addCapability(Status.HOSTILE_TO_PLAYER);
@@ -13,7 +23,10 @@ public class Dog extends Enemy{
 
 
     }
-
+    /**
+     * Creates and returns an intrinsic weapon.
+     * @return A newly instantiated intrinsic weapon
+     */
     @Override
     public IntrinsicWeapon getIntrinsicWeapon() {
         return new IntrinsicWeapon(104, "bites", 93);
