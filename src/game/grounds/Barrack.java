f0d0d1af5f9a2e534f8127529d7eb20c96ab7a89
YewYeePerng <yeeperngyew@gmail.com>
2023-05-13 16:16:15 +0800

    Added new spawn function for Barracks

--- src/game/grounds/Barrack.java
+++ src/game/grounds/Barrack.java
@@ -1,5 +1,8 @@
 package game.grounds;
 
+import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.positions.Location;
+
 /**
  * The Barrack class which is one of the spawning grounds in the game
  * @author Yew Yee Perng
@@ -12,5 +15,15 @@ public class Barrack extends SpawningGrounds{
      */
     public Barrack() {
         super('B');
+    }
+
+    @Override
+    public void spawn(Location location) {
+        Actor enemy = getFactory().createGodrick(location);
+        if (enemy != null) {
+            location.addActor(enemy);
+        }
+
+
     }
 }
