7363213a93d42ed285cfabb0535392f137778cbf
YewYeePerng <yeeperngyew@gmail.com>
2023-05-13 16:50:25 +0800

    Added JavaDocs for PuddleOfWater

--- src/game/grounds/PuddleOfWater.java
+++ src/game/grounds/PuddleOfWater.java
@@ -25,6 +25,10 @@ public class PuddleOfWater extends SpawningGrounds {
         super('~');
     }
 
+    /**
+     * To spawn the enemy at that current location
+     * @param location The location of where the enemy will spawn
+     */
     @Override
     public void spawn(Location location) {
         Actor enemy = getFactory().createCrustaceans(location);
