f0a47b9b3f69bd3610753f69ef7032a91f6b22e1
YewYeePerng <yeeperngyew@gmail.com>
2023-05-13 16:16:27 +0800

    Added new spawn function for Cage

--- src/game/grounds/Cage.java
+++ src/game/grounds/Cage.java
@@ -1,5 +1,8 @@
 package game.grounds;
 
+import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.positions.Location;
+
 /**
  * The Cage class which is one of the spawning grounds in the game
  * @author Yew Yee Perng
@@ -12,5 +15,15 @@ public class Cage extends SpawningGrounds{
      */
     public Cage() {
         super('<');
+    }
+
+    @Override
+    public void spawn(Location location) {
+        Actor enemy = getFactory().createDog(location);
+        if (enemy != null) {
+            location.addActor(enemy);
+        }
+
+
     }
 }
