9b78d71d15d66b3319f45d9225bd25810b1bd795
YewYeePerng <yeeperngyew@gmail.com>
2023-05-13 16:17:58 +0800

    Changed WestEnemyFactory to accommodate the change of EnemyFactory

--- src/game/grounds/WestEnemyFactory.java
+++ src/game/grounds/WestEnemyFactory.java
@@ -10,62 +10,63 @@ import java.lang.reflect.Constructor;
 import java.util.HashMap;
 import java.util.Map;
 
-/**
- * An enemy factory to spawn an enemy at the west
- * @author Tong Jet Kit
- * @see EnemyFactory
- * @version 1.0
- */
-public class WestEnemyFactory extends EnemyFactory{
+public class WestEnemyFactory implements EnemyFactory{
 
-    /**
-     * Constructor for the enemy factory
-     */
+
     public WestEnemyFactory() {
-        HashMap<Character,Actor> enemyInstance = new HashMap<>(){{
-            put('n', new HeavySkeletonSwordsman());
-            put('&', new LoneWolf());
-            put('~', new GiantCrab());
-            put('<', new Dog());
-            put('B', new GodrickSoldier());
-        }};
-        HashMap<Character, Integer> enemySpawnChance = new HashMap<>(){{
-            put('n',27);
-            put('&',33);
-            put('~',2);
-            put('<',37);
-            put('B',45);
-        }};
-
-        this.setEnemySpawnChance(enemySpawnChance);
-        this.setEnemyInstance(enemyInstance);
     }
 
-    /**
-     * To return an enemy to be spawned at a certain location at west
-     * @param location The location where the enemy will be spawning
-     * @param displayChar The display character of the ground
-     * @return An actor which will the enemy to be spawned on the location if granted; null otherwise
-     */
-    public Actor spawnEnemy(Location location, Character displayChar){
-        try {
-            Actor actorToAdd = getEnemyInstance().get(displayChar);
-            Class<?> cls = actorToAdd.getClass();
-            Constructor<?> constructor;
-            constructor = cls.getConstructor();
-            int spawnChance = getEnemySpawnChance().get(displayChar);
-
-            if(RandomNumberGenerator.getRandomInt(100)< spawnChance && !location.containsAnActor()){
-                return (Actor) constructor.newInstance();
-            }
-            else{
-                return null;
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
+    @Override
+    public Actor createSkeleton(Location location) {
+        if(RandomNumberGenerator.getRandomInt(100)< 28 && !location.containsAnActor()){
+            return new HeavySkeletonSwordsman();
         }
-        return null;
+        else{
+            return null;
+        }
     }
+
+    @Override
+    public Actor createCrustaceans(Location location) {
+        if(RandomNumberGenerator.getRandomInt(100)< 3 && !location.containsAnActor()){
+            return new GiantCrab();
+        }
+        else{
+            return null;
+        }
+    }
+
+    @Override
+    public Actor createCanine(Location location) {
+        if(RandomNumberGenerator.getRandomInt(100)< 34 && !location.containsAnActor()){
+            return new LoneWolf();
+        }
+        else{
+            return null;
+        }
+    }
+
+    @Override
+    public Actor createGodrick(Location location) {
+        if(RandomNumberGenerator.getRandomInt(100)< 46 && !location.containsAnActor()){
+            return new GodrickSoldier();
+        }
+        else{
+            return null;
+        }
+    }
+
+    @Override
+    public Actor createDog(Location location) {
+        if(RandomNumberGenerator.getRandomInt(100)< 38 && !location.containsAnActor()){
+            return new Dog();
+        }
+        else{
+            return null;
+        }
+    }
+
+
 
 }
 
