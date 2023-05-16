5fa2e0286147fa4ce6ff6c0851c14fc762741938
YewYeePerng <yeeperngyew@gmail.com>
2023-05-17 00:41:38 +0800

    Created AllowableActions specifically to the conditions of Ally and Invader respectively.

--- src/game/entity/creep/Ally.java
+++ src/game/entity/creep/Ally.java
@@ -10,6 +10,8 @@ import edu.monash.fit2099.engine.weapons.WeaponItem;
 import game.RandomNumberGenerator;
 import game.Species;
 import game.Status;
+import game.action_types.AttackAction;
+import game.behaviours.FollowBehaviour;
 import game.entity.players.Player;
 import game.weapons.playerweapons.Club;
 import game.weapons.playerweapons.GreatKnife;
@@ -48,6 +50,29 @@ public class Ally extends Creep {
             this.addWeaponToInventory(new Club());
         }
 
+    }
+    @Override
+    public ActionList allowableActions(Actor otherActor, String direction, GameMap map) { //unsure
+        ActionList actions = new ActionList();
+        //if this otherActor is a player
+        if(otherActor.hasCapability(Status.HOSTILE_TO_PLAYER)){
+            otherActor.addCapability(Status.IN_COMBAT);
+            actions.add(new AttackAction(this, direction)); // add the intrinsic weapon attack
+
+            // if the player has more than one weapon then add an AttackAction for each weapon
+            if(otherActor.getWeaponInventory().size()!=0){
+                for(WeaponItem weapon: otherActor.getWeaponInventory()){
+                    if(weapon.hasCapability(Status.HAS_ATTACK_SKILL)){ // if this weapon got targeted skill then add the skill action
+                        actions.add(weapon.getSkill(this,direction));
+                    }
+                    actions.add(new AttackAction(this,direction,weapon));
+                }
+            }
+
+
+        }
+
+        return actions;
     }
 
 
--- src/game/entity/creep/Invader.java
+++ src/game/entity/creep/Invader.java
@@ -2,6 +2,7 @@ package game.entity.creep;
 
 import edu.monash.fit2099.engine.actions.Action;
 import edu.monash.fit2099.engine.actions.ActionList;
+import edu.monash.fit2099.engine.actions.DoNothingAction;
 import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.displays.Display;
 import edu.monash.fit2099.engine.positions.GameMap;
@@ -9,12 +10,20 @@ import edu.monash.fit2099.engine.weapons.WeaponItem;
 import game.RandomNumberGenerator;
 import game.Species;
 import game.Status;
+import game.action_types.AttackAction;
+import game.behaviours.Behaviour;
+import game.behaviours.FollowBehaviour;
 import game.weapons.playerweapons.Club;
 import game.weapons.playerweapons.GreatKnife;
 import game.weapons.playerweapons.Staff;
 import game.weapons.playerweapons.Uchigatana;
 
 public class Invader extends Creep {
+
+    /**
+     * A boolean value to determine if the enemy is following a player or not
+     */
+    private boolean isFollow = false;
 
     /**
      * Constructor.
@@ -26,6 +35,7 @@ public class Invader extends Creep {
         this.addCapability(Status.HOSTILE_TO_PLAYER);
         this.addCapability(Species.INVADER);
         this.addCapability(Status.CAN_DROP_RUNES);
+        this.isFollow = false;
     }
 
     public void setCharacter(){
@@ -48,6 +58,32 @@ public class Invader extends Creep {
             this.hitPoints = 414;
             this.addWeaponToInventory(new Club());
         }
+    }
+
+    public ActionList allowableActions(Actor otherActor, String direction, GameMap map) {
+        ActionList actions = new ActionList();
+        //if this otherActor is a player
+        if(otherActor.hasCapability(Status.HOSTILE_TO_ENEMY)){
+
+            otherActor.addCapability(Status.IN_COMBAT);
+            actions.add(new AttackAction(this, direction)); // add the intrinsic weapon attack
+            this.addBehaviour(3,new FollowBehaviour(otherActor)); // let this enemy follow the player
+            this.isFollow = true;
+
+            // if the player has more than one weapon then add an AttackAction for each weapon
+            if(otherActor.getWeaponInventory().size()!=0){
+                for(WeaponItem weapon: otherActor.getWeaponInventory()){
+                    if(weapon.hasCapability(Status.HAS_ATTACK_SKILL)){ // if this weapon got targeted skill then add the skill action
+                        actions.add(weapon.getSkill(this,direction));
+                    }
+                    actions.add(new AttackAction(this,direction,weapon));
+                }
+            }
+
+
+        }
+
+        return actions;
     }
 
 
